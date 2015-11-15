4812abbf50647ebec8f0ccfa722342d13493e6fe
Romain Torond <romain.torond@istyatech.com>
2015-11-15 11:41:06 +0100

    evol

--- js/index.js
+++ js/index.js
@@ -4,9 +4,22 @@ $(document.body).ready(function() {
   
 
   $('#index-list tr').on('click', function(e) {
+
+    //
+    if ( $(e.currentTarget).data('id') == 'eura' ) {
+      $('.main-container-detail[data-id="eura"]').show();
+      $('.main-container-detail[data-id="omnia"]').hide();
+    } else if ( $(e.currentTarget).data('id') == 'omnia' ) {
+      $('.main-container-detail[data-id="omnia"]').show();
+      $('.main-container-detail[data-id="eura"]').hide();
+    } else {
+      return null;
+    }
+
     // slide list
     $('.main-container-list').removeClass('slideIn');
     $('.main-container-list').addClass('slideOut');
+    
     // show icon back
     $('.back-icon').show();
   });
