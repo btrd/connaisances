3209fe6a1518c838ae3b40abcccf07915699cdbf
Romain Torond <romain.torond@istyatech.com>
2015-11-15 12:59:02 +0100

    Compass + points

--- js/index.js
+++ js/index.js
@@ -1,5 +1,36 @@
 
 
+
+getRandomInt = function(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+var startCompass = function(elCompass, pts) {
+  var m = $(elCompass).find('.compass-metre');
+  
+  var count = 10;
+  var processFn = function(previousX) {
+
+    if ( previousX == undefined ) {
+      previousX = 500;
+    }
+    var x = getRandomInt(0, previousX);
+    
+    count--;
+    if (count == 0 || x == 0 ) {
+      elCompass.empty().append('<i class="glyphicon glyphicon-ok"></i><div class="compass-point">+'+pts+'</div>');
+    } else {
+      m.empty().append(x+'m');
+      setTimeout(function() {
+        processFn(x);
+      }, 500);
+    }
+  };
+  
+  processFn();
+};
+
+
 $(document.body).ready(function() {
   
 
@@ -9,9 +40,15 @@ $(document.body).ready(function() {
     if ( $(e.currentTarget).data('id') == 'eura' ) {
       $('.main-container-detail[data-id="eura"]').show();
       $('.main-container-detail[data-id="omnia"]').hide();
+
+      startCompass($('.main-container-detail[data-id="eura"]').find('.compass'), 1000);
+
     } else if ( $(e.currentTarget).data('id') == 'omnia' ) {
       $('.main-container-detail[data-id="omnia"]').show();
       $('.main-container-detail[data-id="eura"]').hide();
+
+      startCompass($('.main-container-detail[data-id="omnia"]').find('.compass'), 2000);
+
     } else {
       return null;
     }
